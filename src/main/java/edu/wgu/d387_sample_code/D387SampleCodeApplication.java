a12884f899a55cf0cefef418393371a3f2a58b3f
Daniel Posada <dposa11@wgu.edu>
2023-10-14 05:40:35 -0500

    Modify D387SampleCodeApplication.java, Lines 7-9 & Lines 15-26: Add new imports & implement thread creation for English and French.

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -4,12 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import edu.wgu.d387_sample_code.international_functions.ShowWelcomeMessage;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(D387SampleCodeApplication.class, args);
+
+		// Create thread for U.S. English
+		ShowWelcomeMessage showWelcomeMessageEnglish = new ShowWelcomeMessage(Locale.US);
+		Thread welcomeThreadEnglish = new Thread(showWelcomeMessageEnglish);
+		welcomeThreadEnglish.start();
+
+		// Create thread for Canadian French
+		ShowWelcomeMessage showWelcomeMessageFrench = new ShowWelcomeMessage(Locale.CANADA_FRENCH);
+		Thread welcomeThreadFrench = new Thread(showWelcomeMessageFrench);
+		welcomeThreadFrench.start();
 	}
 
 }
